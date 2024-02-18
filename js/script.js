97688e0c47444e4515086f95c60389d5e365ec83
Carlos Santos <carlossantosgrochocki@gmail.com>
2024-02-18 15:17:45 -0300

    adicionar-js-perguntas-frequentes

--- js/script.js
+++ js/script.js
@@ -15,7 +15,6 @@ links.forEach(ativarLink);
 //Ativar itens do orçamento
 
 const parametros = new URLSearchParams(location.search);
-console.log(parametros);
 
 function ativarProduto(parametro) {
   const elemento = document.getElementById(parametro);
@@ -25,3 +24,23 @@ function ativarProduto(parametro) {
 }
 
 parametros.forEach(ativarProduto);
+
+// Perguntas frequentes
+
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  pergunta.setAttribute("aria-expanded", ativa);
+}
+
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
